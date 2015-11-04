f3224fe3f5e4d449b89d710e3f75f87f50623200
Christopher Wood <christopherwood07@gmail.com>
2015-11-04 01:41:06 -0800

    simple test update

--- src/simple.py
+++ src/simple.py
@@ -1,23 +1,29 @@
-from test import *
+from pythreatspec import *
 import os
 import sys
 
 class Page(object):
-    def __init__(self, title, body):
+    def __init__(self, title):
         self.title = title
-        self.body = body
+        self.data = None
 
     @mitigates("WebApp", "FileSystem", "unauthorized access", "strict file permissions")
     @exposes("WebApp", "FileSystem", "arbitrary file writes", "insufficient path validation")
     @sends("notification email", "WebApp", "App", "User", "Mail Client")
     def save(self):
-        filename = self.title + ".txt"
-        with open(filename, "r") as handle:
-            handle.write(self.body)
+        filename = self.title
+        if self.data != None:
+            with open(filename, "w") as handle:
+                handle.write(self.body)
 
-@exposes("WebApp", "FileSystem", "arbitrary file reads", "insufficient path validation")
-def loadPage(title):
-    filename = title + ".txt"
-    handle = open(filename, "r")
-    data = handle.read()
-    return Page(title, data)
+    @exposes("WebApp", "FileSystem", "arbitrary file reads", "insufficient path validation")
+    def load(self):
+        filename = self.title
+        handle = open(filename, "r")
+        data = handle.read()
+        self.data = data
+
+if __name__ == "__main__":
+    page = Page("simple.py")
+    page.load()
+    # page.save()
