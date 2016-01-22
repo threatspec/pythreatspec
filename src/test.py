31a759870c68142a52c6e02f6ae27d3e8b0d604f
Christopher Wood <christopherwood07@gmail.com>
2016-01-22 09:15:13 -0800

    moving stuff around

---
+++ src/test.py
@@ -0,0 +1,23 @@
+class Story:
+  '''
+  Represents a book.
+
+  @alias threat @cwe_319_cleartext_transmission to cleartext \
+  transmission of data
+  @describe threat @cwe_319_cleartext_transmission as The software transmits sensitive or \
+    security-critical data in cleartext in a communication channel that can be sniffed by \
+    unauthorized actors
+  @alias boundary @webapp to WebApp
+  @describe boundary @webapp as Customer facing web application
+  @mitigates @webapp:FileSystem against unauthorised access with strict \
+    file permissions (#123)
+  @exposes @webapp:App to XSS injection with insufficient input validation (#567)
+  @transfers @cwe_319_cleartext_transmission to User:Browser with non-sensitive \
+    information
+  @accepts arbitrary file writes to @webapp:FileSystem with filename \
+    restrictions (#666)
+  '''
+  def __init__(self, title, author):
+      self.title = title
+      self.author = author
+}
--- src/test.ts
+++
@@ -1,1 +0,0 @@
-{"boundaries": {}, "models": {"ProjectA": {"0": {"dstboundary": "User", "srccomponent": "App", "dstcomponent": "Mail Client", "message": "notification email", "id": 0, "srcboundary": "WebApp"}}}, "components": {}}
