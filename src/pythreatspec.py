fb82a5fdcbf5d316dbe240db9efcd92af366c49d
Christopher Wood <christopherwood07@gmail.com>
2016-01-23 11:39:21 -0800

    proper regex for alias matching

--- src/pythreatspec.py
+++ src/pythreatspec.py
@@ -31,13 +31,48 @@ class PyThreatspecParser(object):
         self.threats = []
         self.projects = []
 
+        self.aliases = {}
+
         self.node_regex = r'^@(?:alias|describe|mitigates|exposes|transfers|accepts).*$'
+
+        self.parse_table = {}
+        self.parse_table["@alias"] = self._parse_alias
+        self.parse_table["@describe"] = self._parse_describe
+        self.parse_table["@mitigates"] = self._parse_mitigates
+        self.parse_table["@exposes"] = self._parse_exposes
+        self.parse_table["@transfers"] = self._parse_transfers
+        self.parse_table["@accepts"] = self._parse_accepts
+
+    def _parse_alias(self, alias):
+        # TODO: create
+        print "parsing an alias"
+        match = re.match(r'@alias (boundary|component|threat) @(\w+)\b to (\w+)\b', alias, re.M | re.I)
+        if match:
+            target = match[0]
+            tid = match[1]
+            alias = match[2]
+
+            # TODO
+
+    def _parse_describe(self, describe):
+        print "parsing an describe"
+
+    def _parse_mitigates(self, mitigates):
+        print "parsing mitigates"
+
+    def _parse_exposes(self, exposes):
+        print "parsing exposes"
+
+    def _parse_transfers(self, transfers):
+        print "parsing transfers"
+
+    def _parse_accepts(self, accepts):
+        print "parsing accepts"
 
     def _parse_comment(self, comment):
         for match in re.findall(self.node_regex, comment, re.M | re.I): # multiline and ignore case
-            # TODO
-            print "parse here: %s" % (match)
-
+            splits = match.split(" ")
+            self.parse_table[splits[0]](splits[1:])
 
     def _parse_classes(self, module):
         class_definitions = [node for node in module.body if isinstance(node, ast.ClassDef)]
