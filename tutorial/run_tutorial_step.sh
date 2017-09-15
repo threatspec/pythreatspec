f4551d2ab172fbc176ecb10a3e1259de70dae8e2
Fraser Scott <fraser.scott@gmail.com>
2017-09-15 20:47:50 +0100

    supports python file name

--- tutorial/run_tutorial_step.sh
+++ tutorial/run_tutorial_step.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-echo "Running universal parser for $1"
+if [ ${1: -14} == "_components.py" ]; then
+  name=${1:0:16}
+else
+  name=$1
+fi
+
+echo "Running universal parser for $name"
 source ../venv/bin/activate
-../universal.py -p "$1" ${1}_*.py
+../universal.py -p "$name" ${name}_*.py
 deactivate
 
 echo "Running mermaid DFD reporting tool"
 pushd ../../report_dfd
 source venv/bin/activate
-./mermaid.py ../pythreatspec/examples/cwe_library.threatspec.json "../pythreatspec/tutorial/${1}.threatspec.json" > "../pythreatspec/tutorial/${1}.mermaid"
+./mermaid.py ../pythreatspec/examples/cwe_library.threatspec.json "../pythreatspec/tutorial/${name}.threatspec.json" > "../pythreatspec/tutorial/${name}.mermaid"
 deactivate
 popd
 
 echo "Generating PNG using mermaid"
-mermaid -w 1871 "${1}.mermaid"
+mermaid -w 1871 "${name}.mermaid"
 
-echo "You can now view ${1}.mermaid.png"
+echo "You can now view ${name}.mermaid.png"
